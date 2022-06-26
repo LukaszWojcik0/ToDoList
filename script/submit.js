ec8b941e6242cb1ace6c9ab54a4373a4158c44f9
LukaszWojcik3B <73472347+LukaszWojcik3B@users.noreply.github.com>
2022-06-26 12:32:56 +0200

    update25.06

--- script/submit.js
+++ script/submit.js
@@ -1,5 +1,29 @@
 let button = document.querySelector(".inputArea>img");
+let counter = 0;
 
 button.addEventListener("click", function () {
-  document.querySelector(".inputArea").style.display = "none";
+  let task = document.querySelector("#txtinp").value;
+
+  if (task.length > 1) {
+    const taskElement = document.createElement("li");
+    taskElement.classList.add("list_task");
+    taskElement.value = counter;
+    taskElement.innerText = task;
+
+    const parent = document.querySelector(".listToDo");
+    parent.appendChild(taskElement);
+
+    const deleteMark = document.createElement("img");
+    deleteMark.classList.add("task_delete");
+    deleteMark.src = "img/list/cross-mark-black.png";
+    taskElement.appendChild(deleteMark);
+
+    const confirmMark = document.createElement("img");
+    confirmMark.classList.add("task_confirm");
+
+    ++counter;
+    document.querySelector(".inputArea").style.display = "none";
+  } else {
+    alert("Nazwa zadania musi być dłuższa niż 1 znak");
+  }
 });
