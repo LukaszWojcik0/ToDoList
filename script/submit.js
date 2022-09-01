51908b010776d86548b5fe08a473ceda11e239a1
LukaszWojcik3B <wulkasz0@gmail.com>
2022-09-01 15:18:29 +0200

    enter WORKS

--- script/submit.js
+++ script/submit.js
@@ -3,15 +3,14 @@
 let button = document.querySelector(".inputArea>img");
 let counter = 0;
 
-
 function taskSubmit() {
   let task = document.querySelector("#txtinp").value;
-  
+
   if (task.length > 1) {
     // li element
     const taskElement = document.createElement("li");
     taskElement.classList.add("list_task");
-    taskElement.classList.add("orderClass"+counter)
+    taskElement.classList.add("orderClass" + counter);
     taskElement.value = counter;
 
     const parent = document.querySelector(".listToDo");
@@ -22,23 +21,22 @@ function taskSubmit() {
     taskElement_content.classList.add("taskContent");
     taskElement_content.innerText = task;
 
-    const elementParent = document.querySelector(".orderClass"+counter);
+    const elementParent = document.querySelector(".orderClass" + counter);
     elementParent.appendChild(taskElement_content);
 
-
-    
     // icons delete and confirm
     const deleteMark = document.createElement("img");
     deleteMark.classList.add("task_delete");
     deleteMark.src = "img/list/cross-mark-black.png";
     taskElement.appendChild(deleteMark);
-    
+
     const confirmMark = document.createElement("img");
     confirmMark.classList.add("task_confirm");
     confirmMark.src = "img/list/check-mark-black.png";
     taskElement.appendChild(confirmMark);
-    
+
     ++counter;
+
     document.querySelector(".inputArea").style.display = "none";
   } else {
     alert("Nazwa zadania musi być dłuższa niż 1 znak");
@@ -46,9 +44,16 @@ function taskSubmit() {
 }
 
 button.addEventListener("click", taskSubmit);
-addEventListener("keypress", function(e){
-  if (e.key === 'Enter'){
-    taskSubmit
-    console.log("no klikam")
-  }
-})
+
+const addTask = document.querySelector("#mainbox_list > img");
+addTask.addEventListener("click", function () {
+  window.addEventListener("keydown", function (e) {
+    if (
+      e.key === "Enter" &&
+      document.querySelector(".inputArea").style.display == "flex"
+    ) {
+      console.log("essasunia?");
+      taskSubmit();
+    }
+  });
+});
