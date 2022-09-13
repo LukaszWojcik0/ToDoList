3ab4854ce16c803d83276376fcf068339615f0e5
LukaszWojcik3B <wulkasz0@gmail.com>
2022-09-14 00:35:00 +0200

    goodnight ;)

---
+++ script/taskComplete.js
@@ -0,0 +1,12 @@
+/// adding task to local storage and transfering it to 'completed' tab
+
+// function taskLocal() {
+
+// }
+
+localStorage.setItem("zadanko1", "super picko");
+localStorage.setItem("zadanko2", "słabe picko");
+const koko = localStorage.getItem("zadanko1");
+const koko2 = localStorage.getItem("zadanko2");
+console.log(koko);
+console.log(koko2);
