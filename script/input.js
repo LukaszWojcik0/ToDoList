c74b5ae031e1b3fe5e242a5a1462b3a78466b2fb
LukaszWojcik3B <wulkasz0@gmail.com>
2022-10-14 09:05:35 +0200

    fix to fix

--- script/input.js
+++ script/input.js
@@ -4,3 +4,11 @@ let buttonShow = document.querySelector("#mainbox_list > img");
 buttonShow.addEventListener("click", function () {
   document.querySelector(".inputArea").style.display = "flex";
 });
+// localStorage.clear()
+// localStorage.setItem("z0", "zad0")
+// localStorage.setItem("z1", "zad1")
+// localStorage.setItem("z2", "zad2")
+// localStorage.setItem("z3", "zad3")
+// localStorage.setItem("z4", "zad4")
+// localStorage.setItem("z5", "zad5")
+// localStorage.setItem("counter", 6)
--- script/taskDelete.js
+++ script/taskDelete.js
@@ -6,7 +6,7 @@ function DelteTask() {
   let parent = document.querySelector(".listToDo");
   //   valueNumber.removeChild(document.querySelector("." + this.classList[1]));
   parent.removeChild(childLi);
-  localStorage.removeItem("z" + valueNumber); ///?????
+  localStorage.removeItem("z" + valueNumber); 
   let Oldcounter = localStorage.getItem("counter");
   counter = Oldcounter - 1;
   localStorage.removeItem("counter");
@@ -28,6 +28,7 @@ function DelteTask() {
       console.log(i + " null");
 
       for (j = i; j <= counter; j++) {
+        
         document.querySelector(".orderClass" + (j + 1)).value = j;
         document
           .querySelector(".orderClass" + (j + 1))
@@ -35,6 +36,12 @@ function DelteTask() {
         document
           .querySelector(".orderClass" + (j + 1))
           .classList.remove("orderClass" + (j + 1));
+
+          document.querySelector(".del-"+(j+1)).classList.add("del-"+j)
+          document.querySelector(".del-"+(j+1)).classList.remove("del-"+(j+1))
+
+          document.querySelector(".conf-"+(j+1)).classList.add("conf-"+j)
+          document.querySelector(".conf-"+(j+1)).classList.remove("conf-"+(j+1))
       }
     } else {
       console.log(document.querySelector(".orderClass" + i));
