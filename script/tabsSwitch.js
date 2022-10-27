91b5d7c678f6c83c08e414fe7841a32f7482dc07
LukaszWojcik3B <wulkasz0@gmail.com>
2022-10-27 09:03:20 +0200

    changing cards done

--- script/tabsSwitch.js
+++ script/tabsSwitch.js
@@ -7,8 +7,12 @@ taskLabel.addEventListener("click",function(){
     completedLabel.src = ("img/top icons/done/doneNotactive_lightmode.png") 
     document.querySelector("#mainbox_list").style.display= "flex";
     document.querySelector("#mainbox_completed").style.display= "none";
-    document.querySelector("#mainbox_icons_completedbox").style
-    document.querySelector("#mainbox_icons_iconbox").style
+    document.querySelector("#mainbox_icons_completedbox").style.backgroundColor = "#e6e6e6";
+    document.querySelector("#mainbox_icons_iconbox").style.backgroundColor = "#f8f8f8";
+    document.querySelector("#mainbox_icons_iconbox").style.borderBottom = "0px solid #c5c5c5";
+    document.querySelector("#mainbox_icons_completedbox").style.borderBottom = "2px solid #c5c5c5";
+    document.querySelector("#mainbox_icons_iconbox").style.borderRight = "0px solid #c5c5c5";
+    document.querySelector("#mainbox_icons_completedbox").style.borderLeft = "2px solid #c5c5c5";
 })
 
 completedLabel.addEventListener("click",function(){
@@ -16,5 +20,11 @@ completedLabel.addEventListener("click",function(){
     completedLabel.src = ("img/top icons/done/doneActive_lightmode.png")
     document.querySelector("#mainbox_list").style.display= "none";
     document.querySelector("#mainbox_completed").style.display= "flex";
+    document.querySelector("#mainbox_icons_completedbox").style.backgroundColor = "#f8f8f8";
+    document.querySelector("#mainbox_icons_iconbox").style.backgroundColor = "#e6e6e6";;
+    document.querySelector("#mainbox_icons_iconbox").style.borderBottom = "2px solid #c5c5c5";
+    document.querySelector("#mainbox_icons_completedbox").style.borderBottom = "0px solid #c5c5c5";
+    document.querySelector("#mainbox_icons_iconbox").style.borderRight = "2px solid #c5c5c5";
+    document.querySelector("#mainbox_icons_completedbox").style.borderLeft = "0px solid #c5c5c5";
 
 })
